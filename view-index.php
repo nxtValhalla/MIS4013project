2c713b2ffcdaeac6390692772b067dfbb0f6afde
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 03:04:06 -0600

    Update view-index.php

--- view-index.php
+++ view-index.php
@@ -1,9 +1,10 @@
-<!-- Hero Section -->
       <div class="hero">
-        <h1 class="display-4"><?= $pageTitle ?></h1>
-      </div>
+        <h1 class="display-4">About Us</h1>
+            
 <div class="container mt-5">
   <div class="text-center">
     <p>Explore our programs and meet our expert trainers!</p>
   </div>
 </div>
+      </div>
+
