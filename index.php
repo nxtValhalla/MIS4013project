3c3cfd94044d506ed3ac03dc3f312f2018b5352d
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 05:01:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 $pageTitle = "Home Page";
 include "view-header.php";
 ?>
-    <link rel="stylesheet" href="sticky-header.css">
     <link rel="stylesheet" href="background-image.css">
     <link rel="stylesheet" href="page-heading.css">
     <link rel="stylesheet" href="content-box.css">
+    <link rel="stylesheet" href="sticky-header.css">
     <h1 class="page-header">Welcome to ACX Handle Fitness</h1>
 <?php
 include "view-index.php";
