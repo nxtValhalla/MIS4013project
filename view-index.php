42c30e2825522fb0feb2bb7f69451e1d2db20746
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 03:51:51 -0600

    Update view-index.php

--- view-index.php
+++ view-index.php
@@ -1,5 +1,19 @@
 <div class="hero">
       <h1 class="display-4">About Us</h1>
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="container-fluid mt-5">
             <div class="text-center">
                 <p>Explore our programs and meet our expert trainers!</p>
