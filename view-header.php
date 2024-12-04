1fcd03283e3714fbc701d2176fcb1b450b0483d6
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 02:52:40 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,7 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container-fluid">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Directory</a>
