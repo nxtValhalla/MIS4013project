39370fb460b1bed9de5bc234c8f181977b8e481e
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 12:29:17 -0600

    Update programs.php

--- programs.php
+++ programs.php
@@ -1,1 +1,13 @@
-
+<?php
+$pageTitle = "Programs";
+include "view-header.php";
+?>
+    <link rel="stylesheet" href="background-image.css">
+    <link rel="stylesheet" href="page-heading.css">
+    <link rel="stylesheet" href="content-box.css">
+    <link rel="stylesheet" href="sticky-header.css">
+    <h1 class="page-header">Programs</h1>
+<?php
+include "view-programs.php";
+include "view-footer.php";
+?>
