8ffa3692124a484e50f2511093856dc48a2e1e43
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 02:57:42 -0600

    Update view-index.php

--- view-index.php
+++ view-index.php
@@ -1,15 +1,3 @@
-<style>
-      /* Custom styles for header */
-      .hero {
-        background: url('GymBackground.jpg') no-repeat center center/cover;
-        height: 50vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: white;
-        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
-      }
-    </style>
 <!-- Hero Section -->
       <div class="hero">
         <h1 class="display-4"><?= $pageTitle ?></h1>
