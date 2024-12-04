cc427f9f183a0c746922f27e02ab026b53db7a9f
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 04:54:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $pageTitle = "Home Page";
 include "view-header.php";
 ?>
+    <link rel="stylesheet" href="sticky-header.css">
     <link rel="stylesheet" href="background-image.css">
     <link rel="stylesheet" href="page-heading.css">
     <link rel="stylesheet" href="content-box.css">
