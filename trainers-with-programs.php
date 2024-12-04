983829408ca64b65c61c786d31961547a6ba17bc
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 12:31:38 -0600

    Create trainers-with-programs.php

---
+++ trainers-with-programs.php
@@ -0,0 +1,13 @@
+<?php
+$pageTitle = "Who Trains What?";
+include "view-header.php";
+?>
+    <link rel="stylesheet" href="background-image.css">
+    <link rel="stylesheet" href="page-heading.css">
+    <link rel="stylesheet" href="content-box.css">
+    <link rel="stylesheet" href="sticky-header.css">
+    <h1 class="page-header">Programs by Trainer</h1>
+<?php
+include "view-trainers-with-programs.php";
+include "view-footer.php";
+?>
