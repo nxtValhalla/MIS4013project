06ac8baa636528cb2e3546a2803d8044b3979f7f
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 04:12:30 -0600

    Update view-index.php

--- view-index.php
+++ view-index.php
@@ -2,17 +2,17 @@
       <h1 class="display-4">About Us</h1>
       <div class="card mb-3" style="max-width: 600px;">
         <div class="row g-0">
-          <div class="col-md-4">
+          <div class="col-md-3">
             <img src="ACX_SquareLogo2.png" class="img-fluid rounded-start">
           </div>
-          <div class="col-md-8">
+          <div class="col-md-6">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
           </div>
-          <div class="col-md-4">
+          <div class="col-md-3">
             <img src="ACX_SquareLogo2.png" class="img-fluid rounded-start">
           </div>
         </div>
