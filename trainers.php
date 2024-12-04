e1297baf6f387656bb601fccec4554cab7e96f31
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 12:24:31 -0600

    Update trainers.php

--- trainers.php
+++ trainers.php
@@ -1,1 +1,13 @@
-
+<?php
+$pageTitle = "Meet Our Trainers";
+include "view-header.php";
+?>
+    <link rel="stylesheet" href="background-image.css">
+    <link rel="stylesheet" href="page-heading.css">
+    <link rel="stylesheet" href="content-box.css">
+    <link rel="stylesheet" href="sticky-header.css">
+    <h1 class="page-header">Our Trainers</h1>
+<?php
+include "view-trainers.php";
+include "view-footer.php";
+?>
