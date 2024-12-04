84cafe810fbd0bde5bb7b36fb384d59a6974f83c
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-04 02:01:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home Page";
 include "view-header.php";
 ?>
     <link rel="stylesheet" href="page-heading.css">
-    <h1>Welcome to ACX Handle Fitness</h1>
+    <h1 class="page-header">Welcome to ACX Handle Fitness</h1>
 <?php
 include "view-index.php";
 include "view-footer.php";
