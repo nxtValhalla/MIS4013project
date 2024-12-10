dab34f41c9b6b9f4c575c7ba46a5dc46e322196e
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 00:33:37 -0600

    Update view-trainers.php

--- view-trainers.php
+++ view-trainers.php
@@ -6,7 +6,7 @@
                   ?>
                   <div class="col-md-4 col-sm-6 mb-4">
                         <div class="card mx-auto" style="width: 18rem;">
-                          <img src="AXC_SquareLogo2.png" class="card-img-top">
+                          <img src="ACX_SquareLogo2.png" class="card-img-top">
                           <div class="card-body">
                               <h5 class="card-title"><?php echo $trainer['TrainerName']; ?></h5>
                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
