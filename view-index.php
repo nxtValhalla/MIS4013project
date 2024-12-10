db3fc5bf9e8a02a7a541afc674b0dfa156af128f
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-09 23:46:40 -0600

    Update view-index.php

--- view-index.php
+++ view-index.php
@@ -2,18 +2,18 @@
       <h1 class="display-2">About Us</h1>
       </br>
       <div class="card mb-3" style="max-width: 70%;">
-        <div class="row g-0">
-          <div class="col-md-3">
+        <div class="row g-0 align-items-center">
+          <div class="col-md-3 d-flex align-items-center justify-content-center">
             <img src="ACX_SquareLogo2.png" class="img-fluid rounded-start">
           </div>
           <div class="col-md-6">
-            <div class="card-body">
-              <h5 class="card-title justify-content-center">The Origin Story</h5>
-              <p class="card-text justify-content-center">This organization was formed by two students currently attending the University of Oklahoma. Alexie Alexiev and Caled Albirini decided it was time to take their passion for health and exercise to the next level. In order to make that happen, they developed training programs and exercises for people of all experience levels, regardless of their progress in their fitness journey. This website will allow to you access information about these programs, including the goal of each workout plan, the locations for which the training is hosted, and each trainer's contact information. Feel free to reach out to them by accessing their information on the "Meet Our Trainers" page!</p>
+            <div class="card-body text-center">
+              <h5 class="card-title">The Origin Story</h5>
+              <p class="card-text">This organization was formed by two students currently attending the University of Oklahoma. Alexie Alexiev and Caled Albirini decided it was time to take their passion for health and exercise to the next level. In order to make that happen, they developed training programs and exercises for people of all experience levels, regardless of their progress in their fitness journey. This website will allow to you access information about these programs, including the goal of each workout plan, the locations for which the training is hosted, and each trainer's contact information. Feel free to reach out to them by accessing their information on the "Meet Our Trainers" page!</p>
             <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 d-flex align-items-center justify-content-center">
             <img src="ACX_SquareLogo2.png" class="img-fluid rounded-start">
           </div>
         </div>
