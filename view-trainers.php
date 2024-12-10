5fcf2d2d905609ccf7414d0ca39c3b874f56ff3b
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 00:54:57 -0600

    Update view-trainers.php

--- view-trainers.php
+++ view-trainers.php
@@ -9,7 +9,6 @@
                           <img src="ACX_SquareLogo2.png" class="card-img-top">
                           <div class="card-body">
                               <h5 class="card-title"><?php echo $trainer['TrainerName']; ?></h5>
-                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                           </div>
                           <ul class="list-group list-group-flush">
                               <li class="list-group-item"><strong>Specialization:</strong> <?php echo $trainer['Specialization']; ?></li>
