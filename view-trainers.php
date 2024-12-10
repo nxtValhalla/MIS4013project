aa005c09b08755ae0ecc2ce2c86594f4de2d72de
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 00:46:43 -0600

    Update view-trainers.php

--- view-trainers.php
+++ view-trainers.php
@@ -5,7 +5,7 @@
                   while ($trainer = $trainers->fetch_assoc()){
                   ?>
                   <div class="col-md-4 col-sm-6 mb-4">
-                        <div class="card mx-auto" style="width: 18rem;">
+                        <div class="card mx-auto" style="width: 20rem;">
                           <img src="ACX_SquareLogo2.png" class="card-img-top">
                           <div class="card-body">
                               <h5 class="card-title"><?php echo $trainer['TrainerName']; ?></h5>
@@ -13,11 +13,10 @@
                           </div>
                           <ul class="list-group list-group-flush">
                               <li class="list-group-item"><strong>Specialization:</strong> <?php echo $trainer['Specialization']; ?></li>
-                              <li class="list-group-item"><strong>Email:</strong> <?php echo $trainer['Email']; ?></li>
+                              <li class="list-group-item"><strong>Email:</strong> <a href="mailto:<?php echo $trainer['Email']; ?>" class="card-link"><?php echo $trainer['Email']; ?></a></li>
                               <li class="list-group-item"><strong>Phone:</strong> <?php echo $trainer['Phone']; ?></li>
                           </ul>
                           <div class="card-body">
-                              <a href="mailto:<?php echo $trainer['Email']; ?>" class="card-link">Send an Email</a>
                               <a href="#" class="card-link">Another link</a>
                           </div>
                         </div>
