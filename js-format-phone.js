efee62c68d753ecc315e54a060c5c9d49b0fd8fc
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 03:41:20 -0600

    Create js-format-phone.js

---
+++ js-format-phone.js
@@ -0,0 +1,10 @@
+function formatPhoneNumber(input) {
+    let value = input.value.replace(/\D/g, ''); // Remove all non-digit characters
+    if (value.length <= 3) {
+        input.value = '(' + value;
+    } else if (value.length <= 6) {
+        input.value = '(' + value.substring(0, 3) + ') ' + value.substring(3);
+    } else {
+        input.value = '(' + value.substring(0, 3) + ') ' + value.substring(3, 6) + '-' + value.substring(6, 10);
+    }
+}
