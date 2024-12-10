562f5dede7c6bea5f63eb3f2680412c736667058
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 17:22:38 -0600

    Update trainers-with-programs.php

--- trainers-with-programs.php
+++ trainers-with-programs.php
@@ -1,4 +1,7 @@
 <?php
+require_once("util-db.php");
+require_once("model-trainers-with-programs.php");
+
 $pageTitle = "Who Trains What?";
 include "view-header.php";
 ?>
@@ -7,7 +10,9 @@ include "view-header.php";
     <link rel="stylesheet" href="content-box.css">
     <link rel="stylesheet" href="sticky-header.css">
     <h1 class="page-header">Programs by Trainer</h1>
+
 <?php
+$trainers = selectTrainers();
 include "view-trainers-with-programs.php";
 include "view-footer.php";
 ?>
