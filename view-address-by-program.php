1d6cce41d9840d165a52a80612496e0a0e2f3d13
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 04:35:43 -0600

    Create view-address-by-program.php

---
+++ view-address-by-program.php
@@ -0,0 +1,33 @@
+<div class="hero">
+  <h1>Gym Name and Address</h1>
+  <div class="table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+        <th>Gym ID</th>
+        <th>Gym Name</th>
+        <th>Address</th>
+        <th>City</th>
+        <th>State</th>
+        <th>Zip Code</th>
+        </tr>
+      </thead>
+      <tbody>
+  <?php
+  while ($address = $addressbyprogram->fetch_assoc()){
+  ?>
+    <tr>
+      <td><?php echo $address['GymID']; ?></td>
+      <td><?php echo $address['GymName']; ?></td>
+      <td><?php echo $address['Address']; ?></td>
+      <td><?php echo $address['City']; ?></td>
+      <td><?php echo $address['State']; ?></td>
+      <td><?php echo $address['ZipCode']; ?></td>
+    </tr>
+  <?php
+  }
+  ?>
+      </tbody>
+    </table>
+  </div>
+</div>
