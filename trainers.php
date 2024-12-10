7a12645bd1a94f613c5656deec81f7d484219ddb
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-09 21:03:51 -0600

    Update trainers.php

--- trainers.php
+++ trainers.php
@@ -1,4 +1,7 @@
 <?php
+require_once("util-db.php");
+require_once("model-trainers.php");
+
 $pageTitle = "Meet Our Trainers";
 include "view-header.php";
 ?>
@@ -8,6 +11,33 @@ include "view-header.php";
     <link rel="stylesheet" href="sticky-header.css">
     <h1 class="page-header">Our Trainers</h1>
 <?php
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertTrainer($_POST['trainerName'], $_POST['spec'], $_POST['email'], $_POST['phone'])) {
+        echo '<div class="alert alert-success" role="alert">Trainer Added Successfully!</div>';   
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error. No Trainer Added.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateTrainer($_POST['trainerName'], $_POST['spec'], $_POST['email'], $_POST['phone'], $_POST['trainerid'])) {
+        echo '<div class="alert alert-success" role="alert">Trainer Edited Successfully!</div>';   
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error. No Trainer Edited.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteTrainer($_POST['trainerid'])) {
+        echo '<div class="alert alert-success" role="alert">Trainer Deleted Successfully!</div>';   
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error. No Trainer Removed.</div>';
+      }
+      break;
+  }
+}
+
+$trainers = selectTrainers();
 include "view-trainers.php";
 include "view-footer.php";
 ?>
