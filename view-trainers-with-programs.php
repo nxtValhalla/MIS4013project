aad04fc132fa2078b65243cac0bcb64e6ecf294d
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 17:59:01 -0600

    Update view-trainers-with-programs.php

--- view-trainers-with-programs.php
+++ view-trainers-with-programs.php
@@ -3,41 +3,47 @@
 <?php
 while ($trainer = $trainers->fetch_assoc()){
 ?>
-  <div class="row mb-4">
-    <div class="card w-100">
-      <div class="card-body">
-        <h5 class="card-title">
-          ID: <?php echo $trainer['TrainerID']; ?> - Name: <?php echo $trainer['TrainerName']; ?>
-        </h5>
-        <p class="card-text">
-        <ul class="list-group">
-<?php
-  $programs = selectProgramsByTrainer($trainer['TrainerID']);
-  while ($program = $programs->fetch_assoc()) {
-?>     
-    <li class="list-group-item"><?php echo $program['ProgramName']; ?>: <?php echo $program['ProgramDescription']; ?>
-      <form method="post" action="">
-        <input type="hidden" name="programID" value="<?php echo $program['ProgramID'];?>">
-        <input type="hidden" name="actionType" value="Delete">
-        <button type="submit" class="btn btn-danger" onclick="return confirm('Confirm deletion of <?php echo $program['ProgramName']; ?> from <?php echo $trainer['TrainerName']; ?>');">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-          </svg>
-        </button>
-      </form>
-    </li>
-<?php
-  }
-?>
-        </ul>
-        </p>
-        <p class="card-text"><small class="text-body-secondary">Specialization: <?php echo $trainer['Specialization']; ?></small></p>
-      </div>
-    </div>
-  </div>
-<?php
-}
-?>
-</div>
+      <h2 class="mb-3">ID: <?php echo $trainer['TrainerID']; ?> - Name: <?php echo $trainer['TrainerName']; ?></h2>
+      <?php
+        $programs = selectProgramsByTrainer($trainer['TrainerID']);
+        while ($program = $programs->fetch_assoc()) {
+      ?>
+      <h5 class="mb-3">Program Details</h5>
+                  <div class="row text-start">
+                  <div class="col-md-6">
+                        <p><strong>Duration (Weeks):</strong> <?php echo htmlspecialchars($program['Duration']); ?></p>
+                        <p><strong>Days per Week:</strong> <?php echo htmlspecialchars($program['DaysPerWeek']); ?></p>
+                  </div>
+                  <div class="col-md-6">
+                        <p><strong>Difficulty Level (1-7):</strong> <?php echo htmlspecialchars($program['DifficultyLevel']); ?></p>
+                        <p><strong>Intensity (1-7):</strong> <?php echo htmlspecialchars($program['Intensity']); ?></p>
+                  </div>
+                  </div>
+                  <h5 class="mt-3">Description</h5>
+                  <p><?php echo htmlspecialchars($program['ProgramDescription']); ?></p>
+                  <div class="row text-start">
+                        <div class="col-md-4">
+                              <?php include "view-trainers-with-programs-editform.php"; ?>
+                        </div>
+                        <div class="col-md-4">
+                              <form method="post" action="">
+                              <input type="hidden" name="programID" value="<?php echo $program['ProgramID'];?>">
+                              <input type="hidden" name="actionType" value="Delete">
+                              <button type="submit" class="btn btn-danger" onclick="return confirm('Confirm deletion of <?php echo $program['ProgramName'];?> from <?php echo $trainer['TrainerName']; ?>');">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                              </svg>
+                              </button>
+                              </form>
+                        </div>
+                  </div>
+            <?php
+            }
+            ?>
+            </div>
+      <p class="card-text"><small class="text-body-secondary">Specialization: <?php echo $trainer['Specialization']; ?></small></p>
+      <?php
+      }
+      ?>
 </div>
