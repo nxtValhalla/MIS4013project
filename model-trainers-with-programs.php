dca937e36408c2cd11681bf735e9d544dba0b402
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 17:20:29 -0600

    Create model-trainers-with-programs.php

---
+++ model-trainers-with-programs.php
@@ -0,0 +1,32 @@
+<?php
+function selectTrainers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT TrainerID, TrainerName, Specialization, Email, Phone FROM project.trainer;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectProgramsByTrainer($TrainerID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT t.TrainerID, TrainerName, Specialization, ProgramName, ProgramDescription FROM project.trainer t join project.program p on t.TrainerID = p.TrainerID WHERE t.TrainerID = ?");
+        $stmt->bind_param("i", $TrainerID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+?>
