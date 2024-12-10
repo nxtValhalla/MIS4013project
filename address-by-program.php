03e380b746efc39648d09c8b0554f8963b86da0e
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 05:14:38 -0600

    Update address-by-program.php

--- address-by-program.php
+++ address-by-program.php
@@ -4,16 +4,15 @@ require_once("model-address-by-program.php");
 
 $pageTitle = "Gym Address by Program";
 include "view-header.php";
-$addressbyprogram = selectAddressByProgram($_POST['programID']);
 ?>
 
     <link rel="stylesheet" href="background-image.css">
     <link rel="stylesheet" href="page-heading.css">
     <link rel="stylesheet" href="content-box.css">
     <link rel="stylesheet" href="sticky-header.css">
-    <h1 class="page-header">Gym Address For <?php echo $addressbyprogram['ProgramName']; ?></h1>
 
 <?php
+$addressbyprogram = selectAddressByProgram($_POST['programID']);
 include "view-address-by-program.php";
 include "view-footer.php";
 ?>
