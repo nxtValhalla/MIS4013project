ae8b9c5754f273b21418259e20c3295679aa221b
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 01:16:05 -0600

    Update programs.php

--- programs.php
+++ programs.php
@@ -14,21 +14,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertProgram($_POST['teamName'], $_POST['wins'], $_POST['losses'], $_POST['locID'])) {
+      if (insertProgram($_POST['programName'], $_POST['duration'], $_POST['diffLevel'], $_POST['intensity'], $_POST['daysPerWeek'], $_POST['programDescription'], $_POST['trainerID'], $_POST['gymID'])) {
         echo '<div class="alert alert-success" role="alert">Program Added Successfully!</div>';   
       } else {
         echo '<div class="alert alert-danger" role="alert">Error. No Program Added.</div>';
       }
       break;
     case "Edit":
-      if (updateProgram($_POST['teamName'], $_POST['wins'], $_POST['losses'], $_POST['locID'], $_POST['teamid'])) {
+      if (updateProgram($_POST['programName'], $_POST['duration'], $_POST['diffLevel'], $_POST['intensity'], $_POST['daysPerWeek'], $_POST['programDescription'], $_POST['trainerID'], $_POST['gymID'], $_POST['programID'])) {
         echo '<div class="alert alert-success" role="alert">Program Edited Successfully!</div>';   
       } else {
         echo '<div class="alert alert-danger" role="alert">Error. No Program Edited.</div>';
       }
       break;
     case "Delete":
-      if (deleteProgram($_POST['programid'])) {
+      if (deleteProgram($_POST['programID'])) {
         echo '<div class="alert alert-success" role="alert">Program Deleted Successfully!</div>';   
       } else {
         echo '<div class="alert alert-danger" role="alert">Error. No Program Removed.</div>';
