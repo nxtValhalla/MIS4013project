c9609476fc5505efe7a43987a03c413aae9211f7
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 05:47:37 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -8,13 +8,13 @@
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3 sticky-header">
-  <div class="container-fluid d-flex justify-content-between align-items-center">
-    <a class="navbar-brand" href="#">Directory</a>
+  <div class="container-fluid">
+    <a class="navbar-brand me-auto" href="#">Directory</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav mx-auto">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
