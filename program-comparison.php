5c087a45b04a3206e523660fe9e51a8135940a3e
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 19:27:01 -0600

    Update program-comparison.php

--- program-comparison.php
+++ program-comparison.php
@@ -7,7 +7,7 @@ include "view-header.php";
 ?>
     <link rel="stylesheet" href="background-image.css">
     <link rel="stylesheet" href="page-heading.css">
-    <link rel="stylesheet" href="comparison-content-box.css">
+    <link rel="stylesheet" href="content-box.css">
     <link rel="stylesheet" href="sticky-header.css">
     <h1 class="page-header">Program Comparison</h1>
 <?php
