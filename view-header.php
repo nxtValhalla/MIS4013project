c95cfad265f22bb1bbcd7d5ad1ecf18dff8dca2a
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-11 02:05:26 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -13,7 +13,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse mx-auto" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse mx-auto justify-content-center" id="navbarSupportedContent">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
