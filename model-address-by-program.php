318cec6204aeb6f97d5d9bb2e11c39283f728e2b
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-10 21:58:21 -0600

    Update model-address-by-program.php

--- model-address-by-program.php
+++ model-address-by-program.php
@@ -2,7 +2,7 @@
 function selectAddressByProgram($programID) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT p.GymID, ProgramName, GymName, Address, City, State, ZipCode FROM project.program p join project.gym g on p.GymID = g.GymID WHERE p.ProgramID = ?");
+        $stmt = $conn->prepare("SELECT p.GymID, ProgramName, GymName, Address, City, State, ZipCode, Latitude, Longitude FROM project.program p join project.gym g on p.GymID = g.GymID WHERE p.ProgramID = ?");
         $stmt->bind_param("i", $programID);
         $stmt->execute();
         $result = $stmt->get_result();
